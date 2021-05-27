fb51f85032ad00e985491820a3a5984181372987
Fredric <fredric.palmgren@gmail.com>
2021-05-28 00:00:40 +0200

    Moved forecast to new sensor Added verbose property Tidied up code

--- custom_components/brandriskute/sensor.py
+++ custom_components/brandriskute/sensor.py
@@ -31,13 +31,16 @@ __version__ = '1.0.0'
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = 'Brandrisk Ute'
+ICON = ["mdi:fire-alert", "mdi:pine-tree-fire"]
 
 SCAN_INTERVAL = timedelta(hours=6)
+CONF_VERBOSE = "verbose"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME): cv.string,
     vol.Optional(CONF_LATITUDE): cv.latitude,
-    vol.Optional(CONF_LONGITUDE): cv.longitude
+    vol.Optional(CONF_LONGITUDE): cv.longitude,
+    vol.Optional(CONF_VERBOSE): cv.boolean
 })
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
@@ -45,25 +48,23 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     name = config.get(CONF_NAME) if config.get(CONF_NAME) is not None else DEFAULT_NAME
     latitude = config.get(CONF_LATITUDE) if config.get(CONF_LATITUDE) is not None else hass.config.latitude
     longitude = config.get(CONF_LONGITUDE) if config.get(CONF_LONGITUDE) is not None else hass.config.longitude
+    verbose = config.get(CONF_VERBOSE) if config.get(CONF_VERBOSE) is not None else False
 
-    api = BrandriskAPI(longitude, latitude)
+    api = BrandriskAPI(longitude, latitude, verbose)
 
-    add_entities([BrandriskSensor(api, name)], True)
+    add_entities([BrandriskSensor(api, name, ICON)], True)
+
+    # Separate sensor for forecast
+    add_entities([BrandriskForecast(api, name, ICON)], True)
 
 class BrandriskSensor(Entity):
     """Representation of a Brandrisk sensor."""
 
-    def __init__(self, api, name):
+    def __init__(self, api, name, icon):
         """Initialize a Brandrisk sensor."""
         self._api = api
         self._name = name
-        self.attributes = {}
-        self.attributes["current"] = {}
-        self.attributes["forecast"] = []
-        self._icon = []
-        self._icon.append("mdi:fire-alert")
-        self._icon.append("mdi:pine-tree-fire")
-
+        self._icon = icon
     @property
     def name(self):
         """Return the name of the sensor."""
@@ -72,7 +73,7 @@ class BrandriskSensor(Entity):
     @property
     def icon(self):
         """Icon to use in the frontend."""
-        if self._api.current['Grass'] > self._api.current['Wood']:
+        if self._api.current['grassIndex'] > self._api.current['woodIndex']:
             return self._icon[0]
         else:
             return self._icon[1]
@@ -85,10 +86,11 @@ class BrandriskSensor(Entity):
     @property
     def device_state_attributes(self):
         """Return the state attributes of the sensor."""
-        data = {
-            'current': self._api.current,
-            'forecast': self._api.forecast
-        }
+        data = {}
+        keys = list(self._api.current.keys())
+        for index, key in enumerate(keys):
+            data[key] = self._api.current[key]
+        data['prohibition'] = self._api.prohibition
 
         return data
 
@@ -99,21 +101,69 @@ class BrandriskSensor(Entity):
 
     def update(self):
         """Get the latest data from the Brandrisk API."""
-        self._api.update('forecast')
+        self._api.update()
+
+class BrandriskForecast(Entity):
+    """Representation of a Brandrisk forecast sensor."""
+
+    def __init__(self, api, name, icon):
+        """Initialize a Brandrisk forecast sensor."""
+        self._api = api
+        self._name = name
+        self._icon = icon
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name + "_forecast"
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend."""
+        if self._api.current['grassIndex'] > self._api.current['woodIndex']:
+            return self._icon[0]
+        else:
+            return self._icon[1]
+
+    @property
+    def state(self):
+        """Return the state of the device."""
+        return self._api.state
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes of the sensor."""
+
+        return {
+            'forecast': self._api.forecast
+        }
+
+    @property
+    def available(self):
+        """Could the device be accessed during the last update call."""
+        return self._api.available
+
+    def update(self):
+        """Get the latest data from the Brandrisk API."""
+        self._api.update()
 
 class BrandriskAPI:
     """Get the latest data and update the states."""
 
-    URL_CURRENT = "https://brandriskapp.msb.se/RiskService.svc/currentrisk/sv/{lat}|{lon}"
-    URL_FORECAST = "https://brandriskapp.msb.se/RiskService.svc/RiskForecast/sv/{lat}|{lon}"
+    URL_BASE = "https://api.msb.se/brandrisk/v2"
+    URL_CURRENT = "/CurrentRisk/sv/{lat}/{lon}"
+    URL_FORECAST = "/RiskDailyForecast/sv/{lat}/{lon}"
+    URL_PROHIBITION = "/FireProhibition/{lat}/{lon}"
 
-    def __init__(self, longitude, latitude):
+    def __init__(self, longitude, latitude, verbose):
         """Initialize the data object."""
-        
+
         self.lat = latitude
         self.lon = longitude
+        self.verbose = verbose
         self.current = {}
         self.forecast = []
+        self.prohibition = {}
         self.state = ''
         self.available = True
         self.update()
@@ -121,57 +171,65 @@ class BrandriskAPI:
     @Throttle(SCAN_INTERVAL)
     def update(self):
         """Get the latest data from Brandrisk Ute."""
+        self.current = {}
+        self.forecast = []
+        self.prohibition = {}
+
+        _LOGGER.debug("Trying to update")
+
         try:
-            self.current = {}
-            self.forecast = []
-
-            _LOGGER.debug("Trying to update")
-
             # Fetch current risk
-            response = urlopen(self.URL_CURRENT.format(lat=self.lat, lon=self.lon))
-
+            response = urlopen(self.URL_BASE + self.URL_CURRENT.format(lat=self.lat, lon=self.lon))
             data = response.read().decode('utf-8')
             jsondata = json.loads(data)
-            self.current = self.make_object(jsondata)
+            self.state = jsondata['forecast']['riskIndex']
 
+            if self.verbose:
+                self.current = jsondata['forecast']
+            else:
+                self.current['date'] = jsondata['forecast']['date']
+                self.current['riskIndex'] = jsondata['forecast']['riskIndex']
+                self.current['grassIndex'] = jsondata['forecast']['grassIndex']
+                self.current['woodIndex'] = jsondata['forecast']['woodIndex']
+                self.current['riskMessage'] = jsondata['forecast']['riskMessage']
+
+        except Exception as e:
+            _LOGGER.error("Unable to fetch current risk from Brandrisk Ute.")
+            _LOGGER.error(str(e))
+            self.available = False
+
+        try:
             # Fetch forecast
-            response = urlopen(self.URL_FORECAST.format(lat=self.lat, lon=self.lon))
-
+            response = urlopen(self.URL_BASE + self.URL_FORECAST.format(lat=self.lat, lon=self.lon))
             data = response.read().decode('utf-8')
             jsondata = json.loads(data)
 
             for index, element in enumerate(jsondata):
-                self.forecast.append(self.make_object(element))
+                if self.verbose:
+                    self.forecast.append(element)
+                else:
+                    forecast_day = {}
+                    forecast_day['date'] = element['date']
+                    forecast_day['riskIndex'] = element['riskIndex']
+                    forecast_day['grassIndex'] = element['grassIndex']
+                    forecast_day['woodIndex'] = element['woodIndex']
+                    forecast_day['riskMessage'] = element['riskMessage']
+                    self.forecast.append(forecast_day)
 
-            if self.current['Grass'] == 6 or self.current['Wood'] == 6:
-                risk = "Extermt stor risk"
-            elif self.current['Grass'] == 5 or self.current['Wood'] == 5:
-                risk = "Mycket stor risk"
-            elif self.current['Grass'] == 4 or self.current['Wood'] == 4:
-                risk = "Stor risk"
-            elif self.current['Grass'] == 3 or self.current['Wood'] == 3:
-                risk = "Måttlig risk"
-            elif self.current['Grass'] == 2 or self.current['Wood'] == 2:
-                risk = "Liten risk"
-            elif self.current['Grass'] == 1 or self.current['Wood'] == 1:
-                risk = "Mycket liten risk"
-
-            self.state = risk
-
-            self.available = True
         except Exception as e:
-            _LOGGER.error("Unable to fetch data from Brandrisk Ute.")
+            _LOGGER.error("Unable to fetch forecast from Brandrisk Ute.")
             _LOGGER.error(str(e))
             self.available = False
 
-    def make_object(self, element):
-        risk = {}
+        try:
+            # Fetch prohibition
+            response = urlopen(self.URL_BASE + self.URL_PROHIBITION.format(lat=self.lat, lon=self.lon))
+            data = response.read().decode('utf-8')
+            jsondata = json.loads(data)
+            self.prohibition = jsondata['fireProhibition']['status']
 
-        risk['Date'] = element['Date']
-        risk['Grass'] = element['Grass']
-        risk['GrassMsg'] = element['GrassMsg']
-        risk['IssuedDate'] = element['IssuedDate']
-        risk['Wood'] = element['Wood']
-        risk['WoodMsg'] = element['WoodMsg']
-
-        return risk
+            self.available = True
+        except Exception as e:
+            _LOGGER.error("Unable to fetch prohibition from Brandrisk Ute.")
+            _LOGGER.error(str(e))
+            self.available = False
