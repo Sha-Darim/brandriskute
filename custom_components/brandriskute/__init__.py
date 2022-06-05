c63f845c58846af5d559774819749ea4bd510a9e
G Johansson <goran.johansson@shiftit.se>
2022-06-05 22:38:07 +0200

    Clean code

---
+++ custom_components/brandriskute/__init__.py
@@ -0,0 +1,1 @@
+"""The Brandrisk Ute component."""
--- custom_components/brandriskute/sensor.py
+++ custom_components/brandriskute/sensor.py
@@ -10,26 +10,33 @@ sensor:
     prohibition: true
     verbose: false
 """
+from __future__ import annotations
+
 import logging
 import json
 from datetime import timedelta
+from typing import Any
 
 from urllib.request import urlopen
 
 import voluptuous as vol
 
+from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entity import Entity
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (
-    CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME)
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType, StateType
+from homeassistant.components.sensor import (
+    PLATFORM_SCHEMA as PARENT_PLATFORM_SCHEMA,
+    SensorEntity,
+)
+from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, CONF_NAME
 from homeassistant.util import Throttle
 
-__version__ = '1.1.3'
+__version__ = "1.1.3"
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = 'Brandrisk Ute'
+DEFAULT_NAME = "Brandrisk Ute"
 ICON = ["mdi:fire-alert", "mdi:pine-tree-fire", "mdi:fire-off"]
 SCAN_INTERVAL = timedelta(hours=6)
 
@@ -37,63 +44,68 @@ CONF_USE_FORECAST = "forecast"
 CONF_USE_PROHIBITION = "prohibition"
 CONF_VERBOSE = "verbose"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME): cv.string,
-    vol.Optional(CONF_LATITUDE): cv.latitude,
-    vol.Optional(CONF_LONGITUDE): cv.longitude,
-    vol.Optional(CONF_USE_FORECAST): cv.boolean,
-    vol.Optional(CONF_USE_PROHIBITION): cv.boolean,
-    vol.Optional(CONF_VERBOSE): cv.boolean
-})
+PLATFORM_SCHEMA = PARENT_PLATFORM_SCHEMA.extend(
+    {
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_LATITUDE): cv.latitude,
+        vol.Optional(CONF_LONGITUDE): cv.longitude,
+        vol.Optional(CONF_USE_FORECAST, default=True): cv.boolean,
+        vol.Optional(CONF_USE_PROHIBITION, default=True): cv.boolean,
+        vol.Optional(CONF_VERBOSE, default=True): cv.boolean,
+    }
+)
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
     """Set up the Brandrisk sensor."""
-    name = config.get(CONF_NAME) if config.get(CONF_NAME) is not None else DEFAULT_NAME
-    latitude = config.get(CONF_LATITUDE) if config.get(CONF_LATITUDE) is not None else hass.config.latitude
-    longitude = config.get(CONF_LONGITUDE) if config.get(CONF_LONGITUDE) is not None else hass.config.longitude
-    use_forecast = config.get(CONF_USE_FORECAST) if config.get(CONF_USE_FORECAST) is not None else True
-    use_prohibition = config.get(CONF_USE_PROHIBITION) if config.get(CONF_USE_PROHIBITION) is not None else True
-    verbose = config.get(CONF_VERBOSE) if config.get(CONF_VERBOSE) is not None else True
+    name = config[CONF_NAME]
+    latitude = config.get(CONF_LATITUDE, hass.config.latitude)
+    longitude = config.get(CONF_LONGITUDE, hass.config.longitude)
+    use_forecast = config[CONF_USE_FORECAST]
+    use_prohibition = config[CONF_USE_PROHIBITION]
+    verbose = config[CONF_VERBOSE]
     usage = [use_forecast, use_prohibition, verbose]
 
     api = BrandriskAPI(longitude, latitude, usage)
 
-    add_entities([BrandriskSensor(api, name, ICON)], True)
+    entities: list = [BrandriskSensor(api, name, ICON)]
 
     if use_forecast:
-        add_entities([BrandriskForecastSensor(api, name, ICON)], True)
+        entities.append([BrandriskForecastSensor(api, name, ICON)], True)
 
     if use_prohibition:
-        add_entities([BrandriskProhibitionSensor(api, name, ICON)], True)
+        entities.append([BrandriskProhibitionSensor(api, name, ICON)], True)
 
-class BrandriskSensor(Entity):
+    async_add_entities(entities, True)
+
+
+class BrandriskSensor(SensorEntity):
     """Representation of a Brandrisk sensor."""
 
-    def __init__(self, api, name, icon):
+    def __init__(self, api: BrandriskAPI, name: str, icon: list) -> None:
         """Initialize a Brandrisk sensor."""
-        self._api = api
-        self._name = name
+        self._api: BrandriskAPI = api
+        self._attr_name = name
         self._icon = icon
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
 
     @property
-    def icon(self):
+    def icon(self) -> str:
         """Icon to use in the frontend."""
-        if self._api.current['grassIndex'] > self._api.current['woodIndex']:
+        if self._api.current["grassIndex"] > self._api.current["woodIndex"]:
             return self._icon[0]
-        else:
-            return self._icon[1]
+        return self._icon[1]
 
     @property
-    def state(self):
-        """Return the state of the device."""
+    def native_value(self) -> StateType:
         return self._api.state
 
     @property
-    def extra_state_attributes(self):
+    def extra_state_attributes(self) -> dict[str, Any] | None:
         """Return the state attributes of the sensor."""
         data = {}
         keys = list(self._api.current.keys())
@@ -103,83 +115,67 @@ class BrandriskSensor(Entity):
         return data
 
     @property
-    def available(self):
+    def available(self) -> bool:
         """Could the device be accessed during the last update call."""
         return self._api.available
 
-    def update(self):
+    def update(self) -> None:
         """Get the latest data from the Brandrisk API."""
         self._api.update()
 
-class BrandriskForecastSensor(Entity):
+
+class BrandriskForecastSensor(SensorEntity):
     """Representation of a Brandrisk forecast sensor."""
 
-    def __init__(self, api, name, icon):
+    def __init__(self, api: BrandriskAPI, name: str, icon: list) -> None:
         """Initialize a Brandrisk forecast sensor."""
-        self._api = api
-        self._name = name
+        self._api: BrandriskAPI = api
+        self._attr_name = f"{name}_forecast"
         self._icon = icon
 
     @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name + "_forecast"
-
-    @property
-    def icon(self):
+    def icon(self) -> str:
         """Icon to use in the frontend."""
-        if self._api.current['grassIndex'] > self._api.current['woodIndex']:
+        if self._api.current["grassIndex"] > self._api.current["woodIndex"]:
             return self._icon[0]
-        else:
-            return self._icon[1]
+        return self._icon[1]
 
     @property
-    def state(self):
-        """Return the state of the device."""
+    def native_value(self) -> StateType:
         return self._api.state
 
     @property
-    def extra_state_attributes(self):
+    def extra_state_attributes(self) -> dict[str, Any]:
         """Return the state attributes of the sensor."""
 
-        return {
-            'forecast': self._api.forecast
-        }
+        return {"forecast": self._api.forecast}
 
     @property
-    def available(self):
+    def available(self) -> bool:
         """Could the device be accessed during the last update call."""
         return self._api.available
 
-    def update(self):
+    def update(self) -> None:
         """Get the latest data from the Brandrisk API."""
         self._api.update()
 
-class BrandriskProhibitionSensor(Entity):
+
+class BrandriskProhibitionSensor(SensorEntity):
     """Representation of a Brandrisk prohibition sensor."""
 
-    def __init__(self, api, name, icon):
+    def __init__(self, api: BrandriskAPI, name: str, icon: list) -> None:
         """Initialize a Brandrisk prohibition sensor."""
-        self._api = api
-        self._name = name
+        self._api: BrandriskAPI = api
+        self._attr_name = f"{name}_prohibition"
+        self._attr_icon = icon[2]
         self._icon = icon
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name + "_prohibition"
 
     @property
-    def icon(self):
-        """Icon to use in the frontend."""
-        return self._icon[2]
+    def native_value(self) -> StateType:
+        return self._api.prohibition["status"]
 
     @property
-    def state(self):
-        """Return the prohibition state."""
-        return self._api.prohibition['status']
-
-    @property
-    def extra_state_attributes(self):
+    def extra_state_attributes(self) -> dict[str, Any]:
         """Return the state attributes of the sensor."""
         data = {}
         keys = list(self._api.prohibition.keys())
@@ -189,11 +185,11 @@ class BrandriskProhibitionSensor(Entity):
         return data
 
     @property
-    def available(self):
+    def available(self) -> bool:
         """Could the device be accessed during the last update call."""
         return self._api.available
 
-    def update(self):
+    def update(self) -> None:
         """Get the latest data from the Brandrisk API."""
         self._api.update()
 
@@ -206,23 +202,23 @@ class BrandriskAPI:
     URL_FORECAST = "/RiskDailyForecast/sv/{lat}/{lon}"
     URL_PROHIBITION = "/FireProhibition/{lat}/{lon}"
 
-    def __init__(self, longitude, latitude, usage):
+    def __init__(self, longitude: float, latitude: float, usage: list) -> None:
         """Initialize the data object."""
 
         self.lat = latitude
         self.lon = longitude
-        self.use_forecast = usage [0]
+        self.use_forecast = usage[0]
         self.use_prohibition = usage[1]
-        self.verbose = usage [2]
+        self.verbose = usage[2]
         self.current = {}
         self.forecast = []
         self.prohibition = {}
-        self.state = ''
+        self.state = ""
         self.available = True
         self.update()
 
     @Throttle(SCAN_INTERVAL)
-    def update(self):
+    def update(self) -> None:
         """Get the latest data from Brandrisk Ute."""
         self.current = {}
         self.forecast = []
@@ -232,30 +228,35 @@ class BrandriskAPI:
 
         try:
             # Fetch current risk
-            response = urlopen(self.URL_BASE + self.URL_CURRENT.format(lat=self.lat, lon=self.lon))
-            data = response.read().decode('utf-8')
+            response = urlopen(
+                self.URL_BASE + self.URL_CURRENT.format(lat=self.lat, lon=self.lon)
+            )
+            data = response.read().decode("utf-8")
             jsondata = json.loads(data)
-            self.state = jsondata['forecast']['riskIndex']
+            self.state = jsondata["forecast"]["riskIndex"]
 
             if self.verbose:
-                self.current = jsondata['forecast']
+                self.current = jsondata["forecast"]
             else:
-                self.current['date'] = jsondata['forecast']['date']
-                self.current['riskIndex'] = jsondata['forecast']['riskIndex']
-                self.current['grassIndex'] = jsondata['forecast']['grassIndex']
-                self.current['woodIndex'] = jsondata['forecast']['woodIndex']
-                self.current['riskMessage'] = jsondata['forecast']['riskMessage']
+                self.current["date"] = jsondata["forecast"]["date"]
+                self.current["riskIndex"] = jsondata["forecast"]["riskIndex"]
+                self.current["grassIndex"] = jsondata["forecast"]["grassIndex"]
+                self.current["woodIndex"] = jsondata["forecast"]["woodIndex"]
+                self.current["riskMessage"] = jsondata["forecast"]["riskMessage"]
 
-        except Exception as e:
-            _LOGGER.error("Unable to fetch current risk from Brandrisk Ute.")
-            _LOGGER.error(str(e))
+        except Exception as error:
+            _LOGGER.error(
+                "Unable to fetch current risk from Brandrisk Ute %s", str(error)
+            )
             self.available = False
 
         if self.use_forecast:
             try:
                 # Fetch forecast
-                response = urlopen(self.URL_BASE + self.URL_FORECAST.format(lat=self.lat, lon=self.lon))
-                data = response.read().decode('utf-8')
+                response = urlopen(
+                    self.URL_BASE + self.URL_FORECAST.format(lat=self.lat, lon=self.lon)
+                )
+                data = response.read().decode("utf-8")
                 jsondata = json.loads(data)
 
                 for index, element in enumerate(jsondata):
@@ -263,27 +264,32 @@ class BrandriskAPI:
                         self.forecast.append(element)
                     else:
                         forecast_day = {}
-                        forecast_day['date'] = element['date']
-                        forecast_day['riskIndex'] = element['riskIndex']
-                        forecast_day['grassIndex'] = element['grassIndex']
-                        forecast_day['woodIndex'] = element['woodIndex']
-                        forecast_day['riskMessage'] = element['riskMessage']
+                        forecast_day["date"] = element["date"]
+                        forecast_day["riskIndex"] = element["riskIndex"]
+                        forecast_day["grassIndex"] = element["grassIndex"]
+                        forecast_day["woodIndex"] = element["woodIndex"]
+                        forecast_day["riskMessage"] = element["riskMessage"]
                         self.forecast.append(forecast_day)
 
-            except Exception as e:
-                _LOGGER.error("Unable to fetch forecast from Brandrisk Ute.")
-                _LOGGER.error(str(e))
+            except Exception as error:
+                _LOGGER.error(
+                    "Unable to fetch forecast from Brandrisk Ute %s", str(error)
+                )
                 self.available = False
 
         if self.use_prohibition:
             try:
                 # Fetch prohibition
-                response = urlopen(self.URL_BASE + self.URL_PROHIBITION.format(lat=self.lat, lon=self.lon))
-                data = response.read().decode('utf-8')
+                response = urlopen(
+                    self.URL_BASE
+                    + self.URL_PROHIBITION.format(lat=self.lat, lon=self.lon)
+                )
+                data = response.read().decode("utf-8")
                 jsondata = json.loads(data)
-                self.prohibition = jsondata['fireProhibition']
+                self.prohibition = jsondata["fireProhibition"]
                 self.available = True
-            except Exception as e:
-                _LOGGER.error("Unable to fetch prohibition from Brandrisk Ute.")
-                _LOGGER.error(str(e))
+            except Exception as error:
+                _LOGGER.error(
+                    "Unable to fetch prohibition from Brandrisk Ute %s", str(error)
+                )
                 self.available = False
